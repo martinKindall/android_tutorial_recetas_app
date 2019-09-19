e39479f49b32e450c7ddafea511f4b1d91884ea7
martinKindall <mart256@gmail.com>
2019-09-19 18:09:20 -0400

    Created Fragment based on template

---
+++ app/src/main/java/com/example/recetas_cocina_prueba/RecetaMainFragment.kt
@@ -0,0 +1,55 @@
+package com.example.recetas_cocina_prueba
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.recetas_cocina_prueba.dummy.DummyContent
+import com.example.recetas_cocina_prueba.dummy.DummyContent.DummyItem
+
+
+class RecetaMainFragment : Fragment() {
+
+    private var columnCount = 2
+
+    private var listener: OnListFragmentInteractionListener? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_recetarecycler_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = RecetaRecyclerRecyclerViewAdapter(DummyContent.ITEMS, listener)
+            }
+        }
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnListFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+}
---
+++ app/src/main/java/com/example/recetas_cocina_prueba/RecetaRecyclerRecyclerViewAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.recetas_cocina_prueba
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+import com.example.recetas_cocina_prueba.dummy.DummyContent.DummyItem
+
+import kotlinx.android.synthetic.main.recycler_receta.view.*
+
+class RecetaRecyclerRecyclerViewAdapter(
+    private val mValues: List<DummyItem>,
+    private val mListener: OnListFragmentInteractionListener?
+) : RecyclerView.Adapter<RecetaRecyclerRecyclerViewAdapter.ViewHolder>() {
+
+    private val mOnClickListener: View.OnClickListener
+
+    init {
+        mOnClickListener = View.OnClickListener { v ->
+            val item = v.tag as DummyItem
+            // Notify the active callbacks interface (the activity, if the fragment is attached to
+            // one) that an item has been selected.
+            mListener?.onListFragmentInteraction(item)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recycler_receta, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = mValues[position]
+        holder.mIdView.text = item.id
+        holder.mContentView.text = item.content
+
+        with(holder.mView) {
+            tag = item
+            setOnClickListener(mOnClickListener)
+        }
+    }
+
+    override fun getItemCount(): Int = mValues.size
+
+    inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        val mIdView: TextView = mView.item_number
+        val mContentView: TextView = mView.content
+
+        override fun toString(): String {
+            return super.toString() + " '" + mContentView.text + "'"
+        }
+    }
+}
+
+interface OnListFragmentInteractionListener {
+    fun onListFragmentInteraction(item: DummyItem)
+}
