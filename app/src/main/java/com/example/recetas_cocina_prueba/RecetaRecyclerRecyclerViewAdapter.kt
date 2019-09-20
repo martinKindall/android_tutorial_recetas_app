d6140edf243fa3d0137e9c1019d74cefc502a9ed
martinKindall <mart256@gmail.com>
2019-09-19 20:33:48 -0400

    Added Glide and showing images on receta recycler

--- app/src/main/java/com/example/recetas_cocina_prueba/RecetaRecyclerRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recetas_cocina_prueba/RecetaRecyclerRecyclerViewAdapter.kt
@@ -4,10 +4,12 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.recetas_cocina_prueba.data.Receta
-
 import kotlinx.android.synthetic.main.recycler_receta.view.*
+
 
 class RecetaRecyclerRecyclerViewAdapter(
     private val mValues: List<Receta>,
@@ -15,6 +17,7 @@ class RecetaRecyclerRecyclerViewAdapter(
 ) : RecyclerView.Adapter<RecetaRecyclerRecyclerViewAdapter.ViewHolder>() {
 
     private val mOnClickListener: View.OnClickListener
+    private var parent: ViewGroup? = null
 
     init {
         mOnClickListener = View.OnClickListener { v ->
@@ -28,6 +31,7 @@ class RecetaRecyclerRecyclerViewAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.recycler_receta, parent, false)
+        this.parent = parent
         return ViewHolder(view)
     }
 
@@ -40,13 +44,22 @@ class RecetaRecyclerRecyclerViewAdapter(
             tag = item
             setOnClickListener(mOnClickListener)
         }
+
+        this.parent?.let {
+            Glide
+                .with(it)
+                .load("https://tacos10.com/storage/2018/12/Tortillas-para-tacos-mexicanos.jpg")
+                .centerCrop()
+                .into(holder.mImage)
+        }
     }
 
     override fun getItemCount(): Int = mValues.size
 
     inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
-        val mIdView: TextView = mView.item_number
-        val mContentView: TextView = mView.content
+        val mIdView: TextView = mView.titulo
+        val mContentView: TextView = mView.descripcion
+        val mImage: ImageView = mView.receta_imagen
 
         override fun toString(): String {
             return super.toString() + " '" + mContentView.text + "'"
