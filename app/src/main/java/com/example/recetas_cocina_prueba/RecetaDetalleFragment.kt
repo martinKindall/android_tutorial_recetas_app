8f48a3990fd9c12797eed0282970e8c34a319a82
martinKindall <mart256@gmail.com>
2019-09-19 21:27:54 -0400

    receta_detalle: view is made as a scrollview, now it can be scrolled+

--- app/src/main/java/com/example/recetas_cocina_prueba/RecetaDetalleFragment.kt
+++ app/src/main/java/com/example/recetas_cocina_prueba/RecetaDetalleFragment.kt
@@ -7,8 +7,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
 import com.example.recetas_cocina_prueba.data.Receta
 import com.example.recetas_cocina_prueba.viewModels.RecetaViewModel
 
@@ -30,8 +33,23 @@ class RecetaDetalleFragment : Fragment() {
 
         val mainView = inflater.inflate(R.layout.fragment_receta_detalle, container, false)
 
-        recetaViewModel.recetaSeleccionada.observe(this, Observer<Receta> {
-            Log.d("mis_recetas", "la receta recibida es: " + it.nombre)
+        recetaViewModel.recetaSeleccionada.observe(this, Observer<Receta> { receta ->
+            Log.d("mis_recetas", "la receta recibida es: " + receta.nombre)
+
+            val imagen = mainView.findViewById<ImageView>(R.id.receta_detalle_imagen)
+
+            context?.let { context ->
+                Glide
+                    .with(context)
+                    .load(receta.link)
+                    .centerCrop()
+                    .into(imagen)
+            }
+
+            mainView.findViewById<TextView>(R.id.receta_detalle_titulo)
+                .text = receta.nombre
+            mainView.findViewById<TextView>(R.id.receta_detalle_descripcion)
+                .text = receta.descripcion
         })
 
         return mainView
